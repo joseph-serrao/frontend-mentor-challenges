42cc9853aa7a10200172514393f0eba295db810d
joseph-serrao <josephserrao42@gmail.com>
2022-10-03 14:46:47 +0530

    Completed mobile design

--- faq-accordion-card/js/index.js
+++ faq-accordion-card/js/index.js
@@ -2,3 +2,20 @@ $('.container').click(function () {
     $('.container').not(this).removeClass("active");
     $(this).toggleClass("active");
 });
+
+$(document).ready(function () {
+    if (window.innerWidth <= 720) {
+        $('.desktop').addClass("hidden");
+        $('.mobile').removeClass("hidden");
+    }
+});
+
+$(window).resize(function () {
+    if (window.innerWidth <= 720) {
+        $('.desktop').addClass("hidden");
+        $('.mobile').removeClass("hidden");
+    } else {
+        $('.mobile').addClass("hidden");
+        $('.desktop').removeClass("hidden");
+    }
+});
